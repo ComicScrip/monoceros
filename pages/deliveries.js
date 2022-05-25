635ee93c125d297cb6e0f85de0daf180687d3bd6
JBillault <billaultjonathan@gmail.com>
2022-05-25 17:12:07 +0200

    trying to call the API and get delivery list, not working

--- pages/deliveries.js
+++ pages/deliveries.js
@@ -1,6 +1,7 @@
 import React from "react";
 import dynamic from "next/dynamic";
 import deliveriesStyle from "../styles/deliveries.module.css";
+import DeliveryList from "../components/deliveryList";
 
 export default function Deliveries() {
   const MapWithNoSSR = dynamic(() => import("../components/map"), {
@@ -13,28 +14,7 @@ export default function Deliveries() {
       <div className={deliveriesStyle.map}>
         <MapWithNoSSR />
       </div>
-      <table className={deliveriesStyle.table}>
-        <thead>
-          <tr>
-            <th className={deliveriesStyle.tHeader}>ID</th>
-            <th className={deliveriesStyle.tHeader}>Status</th>
-            <th className={deliveriesStyle.tHeader}>Alert</th>
-            <th className={deliveriesStyle.tHeader}>Ref.</th>
-            <th className={deliveriesStyle.tHeader}>Destination</th>
-            <th className={deliveriesStyle.tHeader}>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className={deliveriesStyle.tCell}>12</td>
-            <td className={deliveriesStyle.tCell}>In progess</td>
-            <td className={deliveriesStyle.tCell}>Vert</td>
-            <td className={deliveriesStyle.tCell}>JB</td>
-            <td className={deliveriesStyle.tCell}>Bora Bora</td>
-            <td className={deliveriesStyle.tCell}>24/05</td>
-          </tr>
-        </tbody>
-      </table>
+      <DeliveryList />
     </main>
   );
 }
