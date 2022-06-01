b2bbe0983683b42db6349fd12d66515827957f20
01adrien <adrien.go@outlook.fr>
2022-06-01 13:54:10 +0200

    finalLogin

--- components/deliveriesStatus.js
+++ components/deliveriesStatus.js
@@ -9,36 +9,57 @@ export default function DeliveriesStatus({
   const inProgressWidth = Math.ceil(unit * inProgress);
   const delayedWidth = Math.ceil(unit * delayed);
   const completedWidth = Math.ceil(unit * completed);
+  console.log(process.env);
   return (
-    <div className="flex flex-col w-4/6">
-      <div className="mb-[10px] flex items-center justify-between">
-        <p>In progress{inProgress}</p>
-        <div className={`bg-[#eae5e5] w-[${containerWidth}%] h-[15px] rounded`}>
+    <>
+      <div className="flex flex-col w-5/6 mt-10">
+        <h1 className="mb-[15px] text-lg font-bold">My deliveries</h1>
+        <div className="mb-[15px] flex items-center justify-between">
+          <p>In progress</p>
           <div
-            style={{ width: `${inProgressWidth}%` }}
-            className="h-[15px] bg-red-600 rounded"
-          ></div>
+            className={`bg-[#eae5e5] w-[${containerWidth}%] h-[15px] rounded`}
+          >
+            <div
+              style={{ width: `${inProgressWidth}%` }}
+              className="h-[15px] bg-red-600 rounded flex justify-end"
+            >
+              <span className="relative bottom-5 text-red-600">
+                {inProgress}
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="mb-[15px] flex items-center justify-between">
+          <p>Delayed</p>
+          <div
+            className={`bg-[#eae5e5] w-[${containerWidth}%] h-[15px] rounded`}
+          >
+            <div
+              style={{ width: `${delayedWidth}%` }}
+              className="h-[15px] bg-red-600 rounded flex justify-end"
+            >
+              <span className="relative bottom-5 text-red-600">{delayed}</span>
+            </div>
+          </div>
+        </div>
+        <div className="mb-[15px] flex items-center justify-between">
+          <p>Completed</p>
+          <div className="bg-[#eae5e5] w-[70%] h-[15px] rounded ">
+            <div
+              style={{ width: `${completedWidth}%` }}
+              className="h-[15px] bg-red-600 rounded flex justify-end"
+            >
+              <span className="relative bottom-5 text-red-600">
+                {completed}
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center justify-between">
+          <p>Total</p>
+          <span className="text-red-600 w-[70%]">{total}</span>
         </div>
       </div>
-      <div className="mb-[10px] flex items-center justify-between">
-        <p>Delayed{delayed}</p>
-        <div className="bg-[#eae5e5] w-[70%] h-[15px] rounded">
-          <div
-            style={{ width: `${delayedWidth}%` }}
-            className="h-[15px] bg-red-600 rounded"
-          ></div>
-        </div>
-      </div>
-      <div className="mb-[10px] flex items-center justify-between">
-        <p>Completed{completed}</p>
-        <div className="bg-[#eae5e5] w-[70%] h-[15px] rounded">
-          <div
-            style={{ width: `${completedWidth}%` }}
-            className="h-[15px] bg-red-600 rounded"
-          ></div>
-        </div>
-      </div>
-      <p>Total {total}</p>
-    </div>
+    </>
   );
 }
