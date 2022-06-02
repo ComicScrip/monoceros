38f805a59b715470ba3c13f5b4e8daff7688b118
01adrien <adrien.go@outlook.fr>
2022-06-02 14:05:08 +0200

    add custom hook

--- hooks/useApiCall.js
+++ hooks/useApiCall.js
@@ -6,7 +6,7 @@ export default function useApiCall(token, request) {
   const [error, setError] = useState("");
   const [loaded, setLoaded] = useState(false);
   const [isRefreshToken, setIsRefreshToken] = useState(false);
-  const [token, setToken] = useState(token);
+  const [tokens, setToken] = useState(token);
 
   useEffect(() => {
     const loadAsync = async () => {
@@ -38,7 +38,5 @@ export default function useApiCall(token, request) {
     loadAsync();
   }, []);
 
-  return { data, error, loaded, token };
+  return { data, error, loaded, tokens };
 }
-
-const { data, error, loaded, token } = useApiCall(token, getAllProducts);
--- pages/inProgress.js
+++ pages/inProgress.js
@@ -1,7 +1,20 @@
 import Image from "next/image";
 import logo from "../public/images/logo-monoceros.png";
+import tokenContext from "../lib/tokenContext";
+import { useContext } from "react";
+import {
+  getAllProducts,
+  getCurrentUserInfos,
+  getOneDelivery,
+  getDeliveriesOverview,
+} from "../lib";
+import useApiCall from "../hooks/useApiCall";
 
 export default function InProgress() {
+  const { setToken, tokens } = useContext(tokenContext);
+  console.log(tokens);
+  const { data, error, loaded, token } = useApiCall(tokens, getAllProducts);
+  console.log(data);
   return (
     <div>
       <h1 className="text-center mt-8">
