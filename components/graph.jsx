b1029c23a867bf3e28630383f925153ebfc0f316
JBillault <billaultjonathan@gmail.com>
2022-06-08 14:37:17 +0200

    fetching more data

--- components/graph.jsx
+++ components/graph.jsx
@@ -21,41 +21,46 @@ ChartJS.register(
   Legend
 );
 
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: "top",
-    },
-    title: {
-      display: true,
-      text: "Temperature",
-    },
-  },
-};
-
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: "Dataset 1",
-      data: [23, 28, 12, 9, 25, 18, 22],
-      borderColor: "rgb(255, 99, 132)",
-      backgroundColor: "rgba(255, 99, 132, 0.5)",
-    },
-    {
-      label: "Dataset 2",
-      data: [15, 35, 22, 11, 32, 34, 40],
-      borderColor: "rgb(53, 162, 235)",
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
-    },
-  ],
-};
-
 const Graph = () => {
-  return <Line options={options} data={data} width={"80vw"} height={"50vh"} />;
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Temperature",
+      },
+    },
+  };
+  const labels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+  ];
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: [23, 28, 12, 9, 25, 18, 22],
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "Dataset 2",
+        data: [15, 35, 22, 11, 32, 34, 40],
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
+  return <Line options={options} data={data} width={"80%"} height={"50%"} />;
 };
 
 export default Graph;
--- components/groupData.jsx
+++ components/groupData.jsx
@@ -5,13 +5,22 @@ import Graph from "./graph";
 
 const GroupData = () => {
   const [temperatureData, setTemperatureData] = useState([]);
+  const [humidityData, setHumidityData] = useState([]);
+  const [lightData, setLightData] = useState([]);
+  const [vibrationData, setVibrationData] = useState([]);
   function getData() {
-    getSensorData().then(setTemperatureData);
+    getSensorData("177", "225", "temperature").then(setTemperatureData);
+    getSensorData("177", "225", "humidity").then(setHumidityData);
+    getSensorData("177", "225", "shock").then(setVibrationData);
+    getSensorData("177", "225", "light").then(setLightData);
   }
   useEffect(() => {
     getData();
   }, []);
-  console.log("data", temperatureData);
+  console.log("temp", temperatureData);
+  console.log("hum", humidityData);
+  console.log("light", lightData);
+  console.log("shock", vibrationData);
   return (
     <div className={groupDataStyle.container}>
       <div className={groupDataStyle.head}>
