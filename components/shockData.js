cf734fda447d9858107c7f670e7f3bbde62e9525
01adrien <adrien.go@outlook.fr>
2022-06-16 13:24:44 +0200

    removed old code

--- components/shockData.js
+++ components/shockData.js
@@ -3,26 +3,7 @@ import sensorDataStyle from "../styles/sensorData.module.css";
 import { getSensorData } from "../lib/deliveriesAPI";
 import { useEffect, useState } from "react";
 
-const ShockData = ({ deliveryId, packages }) => {
-  const [sensorsData, setSensorsData] = useState([]);
-  const sensors = ["temperature", "light", "shock", "humidity"];
-
-  useEffect(() => {
-    console.log(deliveryId);
-    console.log(packages);
-    async function getData() {
-      for (const dataType of sensors) {
-        //console.log(dataType);
-        const data = await getSensorData(deliveryId, packages[0].id, dataType);
-        //console.log(data);
-
-        setSensorsData([...sensorDataStyle, data[0].sensor_value]);
-        console.log([dataType, data[0].sensor_value]);
-      }
-    }
-    getData();
-  }, []);
-  console.log(sensorsData);
+const ShockData = () => {
   return (
     <div className={sensorDataStyle.global}>
       <h3 className={sensorDataStyle.titre}>Shock</h3>
