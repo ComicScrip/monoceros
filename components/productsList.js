729c17bdee4fa2384312a5dd577940dbd5a3a33e
01adrien <adrien.go@outlook.fr>
2022-06-21 16:06:31 +0200

    before merge

--- components/productsList.js
+++ components/productsList.js
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import {
+  getAllProducts,
   getAllCountries,
   getWarehouses,
+  getProductsByCountry,
   getProductsByCountryAndWarehouse,
+  getProductsByWarehouse,
 } from "../lib/productsAPI";
 import CountrySelect from "./countrySelect";
 import WarehouseSelect from "./warehouseSelect";
@@ -11,47 +14,71 @@ import { useRouter } from "next/router";
 import { useTranslation } from "next-i18next";
 
 export default function ProductsList() {
-  const { t } = useTranslation("productsCatalogue");
+  const { t } = useTranslation("products");
   const router = useRouter();
-  const [countriesList, setCountriesList] = useState([]);
-  const [warehousesList, setWarehousesList] = useState([]);
-  const [products, setProducts] = useState([]);
-  const [queryParams, setQueryParams] = useState({});
-  const [currentPage, setCurrentPage] = useState(
-    parseInt(router.query.page) || 1
-  );
-  const productsPerPage = 10;
-  const [numberOfProducts, setNumberOfProducts] = useState(null);
   const [countrySelect, setCountrySelect] = useState(
     router.query.country || ""
   );
   const [warehouseSelect, setWareHouseSelect] = useState(
     router.query.warehouse || ""
   );
+  const [countriesList, setCountriesList] = useState([]);
+  const [warehousesList, setWarehousesList] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 10;
+  const [numberOfProducts, setNumberOfProducts] = useState(null);
 
   useEffect(() => {
-    console.log(queryParams);
     router.replace({
       query: {
         ...router.query,
         warehouse: warehouseSelect,
         country: countrySelect,
-        page: currentPage,
       },
     });
     async function request() {
-      const products = await getProductsByCountryAndWarehouse(
-        countrySelect,
-        warehouseSelect,
-        productsPerPage,
-        (currentPage - 1) * (productsPerPage + 1)
-      );
-      setNumberOfProducts(products.data.count);
-      setProducts(products.data.results);
-      const warehouses = await getWarehouses(countrySelect);
-      setWarehousesList(warehouses.data);
-      const countries = await getAllCountries();
-      setCountriesList(countries.data);
+      if (!warehouseSelect && !countrySelect) {
+        const products = await getAllProducts(
+          productsPerPage,
+          (currentPage - 1) * (productsPerPage + 1)
+        );
+        setNumberOfProducts(products.data.count);
+        setProducts(products.data.results);
+        const warehouses = await getWarehouses();
+        setWarehousesList(warehouses.data);
+        const countries = await getAllCountries();
+        setCountriesList(countries.data);
+      } else if (countrySelect && !warehouseSelect) {
+        const warehousesList = await getWarehouses(countrySelect);
+        setWarehousesList(warehousesList.data);
+        const products = await getProductsByCountry(
+          countrySelect,
+          productsPerPage,
+          (currentPage - 1) * (productsPerPage + 1)
+        );
+        setNumberOfProducts(products.data.count);
+        setProducts(products.data.results);
+      } else if (warehouseSelect && !countrySelect) {
+        const products = await getProductsByWarehouse(
+          warehouseSelect,
+          productsPerPage,
+          (currentPage - 1) * (productsPerPage + 1)
+        );
+        setNumberOfProducts(products.data.count);
+        setProducts(products.data.results);
+      } else if (warehouseSelect && countrySelect) {
+        const warehousesList = await getWarehouses(countrySelect);
+        setWarehousesList(warehousesList.data);
+        const products = await getProductsByCountryAndWarehouse(
+          countrySelect,
+          warehouseSelect,
+          productsPerPage,
+          (currentPage - 1) * (productsPerPage + 1)
+        );
+        setNumberOfProducts(products.data.count);
+        setProducts(products.data.results);
+      }
     }
     request();
   }, [countrySelect, warehouseSelect, currentPage]);
@@ -86,91 +113,87 @@ export default function ProductsList() {
               selectCountry={setCountrySelect}
               country={countrySelect}
               setCurrentPage={setCurrentPage}
-              setQueryParams={setQueryParams}
-              queryParams={queryParams}
             />
             <WarehouseSelect
               warehouses={warehousesList}
               selectWharehouse={setWareHouseSelect}
               warehouse={warehouseSelect}
               setCurrentPage={setCurrentPage}
-              setQueryParams={setQueryParams}
-              queryParams={queryParams}
             />
           </div>
         </div>
         <div className="w-[95vw] bg-white flex flex-col items-center">
-          {products.length ? (
-            <div
-              className="overflow-x-scroll w-[100%]"
-              style={{ backgroundColor: "var(--main-bg-color)" }}
-            >
-              <table data-cy={"products-table"} className="w-[95vw]">
-                <tbody className="bg-white">
-                  <tr
-                    className="text-[0.6rem] font-bold"
-                    style={{ backgroundColor: "var(--main-bg-color)" }}
-                  >
-                    <td className="min-w-[80px] absolute bg-[#efefef]">
-                      <span>{tableHead[0]}</span>
+          <div
+            className="overflow-x-scroll w-[100%]"
+            style={{ backgroundColor: "var(--main-bg-color)" }}
+          >
+            <table data-cy={"products-table"} className="w-[95vw]">
+              <tbody className="bg-white">
+                <tr
+                  className="text-[0.6rem] font-bold"
+                  style={{ backgroundColor: "var(--main-bg-color)" }}
+                >
+                  <td className="min-w-[100px] absolute bg-[#efefef]">
+                    <span>{tableHead[0]}</span>
+                  </td>
+                  <td></td>
+                  {tableHead.slice(1, 8).map((item) => (
+                    <td className="min-w-[100px]" key={item}>
+                      {item}
                     </td>
-                    <td></td>
-                    {tableHead.slice(1, 9).map((item) => (
-                      <td className="min-w-[100px]" key={item}>
-                        {item}
-                      </td>
-                    ))}
-                  </tr>
-                  {products.map((product, _) => (
-                    <tr
-                      key={_}
-                      className="border-8 font-bold text-[10px] h-16"
-                      style={{ borderColor: "var(--main-bg-color)" }}
-                    >
-                      <td className="min-w-[80px]"></td>
-                      <td
-                        style={{ color: "var(--main-color)" }}
-                        className="min-w-[90px] bg-white absolute flex items-center justify-center text-[0.7rem] left-2 h-14"
-                      >
-                        <span>{product.name}</span>
-                      </td>
-                      <td className="min-w-[90px]">
-                        {product.expiration_date
-                          ? product.expiration_date.slice(0, 10)
-                          : ""}
-                      </td>
-                      <td data-cy={"temp-scale"} className="min-w-[90px]">
-                        {product.temperature_min} / {product.temperature_max}
-                      </td>
-                      <td className="min-w-[70px]">
-                        {product.humidity_min} / {product.humidity_max}
-                      </td>
-                      <td className="min-w-[70px]">
-                        {product.light_min} / {product.light_max}
-                      </td>
-                      <td className="min-w-[70px]">
-                        {product.shock_min} / {product.shock_max}
-                      </td>
-                      <td className="min-w-[70px]">
-                        {product.orientation_cfg}
-                      </td>
-                      <td className="min-w-[70px]">{product.unit_cost}</td>
-                      <td className="min-w-[70px]">
-                        {product.lead_time_average
-                          ? product.lead_time_average
-                          : 0}
-                      </td>
-                    </tr>
                   ))}
-                </tbody>
-              </table>
+                </tr>
+                {products.map((product, _) => (
+                  <tr
+                    key={_}
+                    className="border-8 font-bold text-[10px] h-16"
+                    style={{ borderColor: "var(--main-bg-color)" }}
+                  >
+                    <td className="min-w-[100px]"></td>
+                    <td
+                      style={{ color: "var(--main-color)" }}
+                      className="min-w-[120px] bg-white absolute flex items-center justify-center text-[0.7rem] left-2 h-14"
+                    >
+                      <span>{product.name}</span>
+                    </td>
+                    <td className="min-w-[90px]">
+                      {product.expiration_date
+                        ? product.expiration_date.slice(0, 10)
+                        : ""}
+                    </td>
+                    <td
+                      data-cy={"temp-scale"}
+                      className="min-w-[90px] content-center"
+                    >
+                      {product.temperature_min} / {product.temperature_max}
+                    </td>
+                    <td className="min-w-[90px]">
+                      {product.humidity_min} / {product.humidity_max}
+                    </td>
+                    <td className="min-w-[90px]">
+                      {product.light_min} / {product.light_max}
+                    </td>
+                    <td className="min-w-[90px]">
+                      {product.shock_min} / {product.shock_max}
+                    </td>
+                    <td className="min-w-[90px]">X</td>
+                    <td className="min-w-[90px]">{product.unit_cost}</td>
+                    <td className="min-w-[90px]">
+                      {product.lead_time_average
+                        ? product.lead_time_average
+                        : 0}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+
+          {products.length < 1 ? (
+            <div className="flex items-center justify-center bg-white w-[90vw] h-16">
+              <p>No products</p>
             </div>
           ) : (
-            <div className="flex items-center justify-center bg-white w-[90vw] h-16">
-              <p style={{ color: "var(--main-color)" }}>{t("noData")}</p>
-            </div>
-          )}
-          {Math.ceil(numberOfProducts / productsPerPage) > 1 ? (
             <div
               className="flex justify-center w-full"
               style={{ backgroundColor: "var(--main-bg-color)" }}
@@ -179,10 +202,9 @@ export default function ProductsList() {
                 index={Math.ceil(numberOfProducts / productsPerPage)}
                 setCurrentPage={setCurrentPage}
                 currentPage={currentPage}
-                setQueryParams={setQueryParams}
               />
             </div>
-          ) : null}
+          )}
         </div>
       </div>
     </>
