6b4316ae995cdcafd8f3ab0dc526970ed9d80879
Jennifer DE LEO <deleo.jennifer1@gmail.com>
2022-07-05 12:24:50 +0200

    first commit

--- pages/alarms/index.js
+++ pages/alarms/index.js
@@ -1,32 +1,20 @@
-import Image from "next/image";
 import Meta from "../../components/meta";
 import Layout from "../../components/layout";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
+import { MdLightMode } from "react-icons/md";
+import { RiTempColdLine } from "react-icons/ri";
+import { IoWater } from "react-icons/io";
+import { AiOutlineDashboard } from "react-icons/ai";
+import { GiHandcuffed } from "react-icons/gi";
+import { BsFillCalendarXFill } from "react-icons/bs";
 
 const Alarms = () => {
-  const { t } = useTranslation("common");
+  const { t } = useTranslation("alarms");
   return (
     <Layout>
-      <Meta pagetitle="Monoceros - Alarms" />
-      <h1>Alarms</h1>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Image
-          priority
-          src="/images/enconstruction.png"
-          height={200}
-          width={200}
-          alt="en construction"
-        />
-        <p>{t("description")}</p>
-      </div>
+      <Meta pagetitle={t("pageTitle")} />
+      <h1>{t("title")}</h1>
     </Layout>
   );
 };
@@ -36,7 +24,12 @@ export default Alarms;
 export async function getStaticProps({ locale }) {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ["common", "home", "navbar"])),
+      ...(await serverSideTranslations(locale, [
+        "common",
+        "home",
+        "navbar",
+        "alarms",
+      ])),
     },
   };
 }
