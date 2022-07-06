f3aab4c0030cb40506dc4a9d89e6a7011baac454
Jennifer DE LEO <deleo.jennifer1@gmail.com>
2022-07-06 12:35:10 +0200

    filter by country, warehouse, product

--- components/alarmsList.js
+++ components/alarmsList.js
@@ -3,7 +3,10 @@ import Pagination from "./pagination";
 import Loading from "./loading";
 import { useRouter } from "next/router";
 import { useTranslation } from "next-i18next";
-import { getAlarms } from "../lib/alarmsAPI";
+import {
+  getAlarms,
+  getAlarmsByCountryWarehouseAndProduct,
+} from "../lib/alarmsAPI";
 import { MdLightMode, MdWaterDrop } from "react-icons/md";
 import { RiTempColdLine } from "react-icons/ri";
 import { AiOutlineDashboard } from "react-icons/ai";
@@ -11,39 +14,101 @@ import { GiHandcuffed } from "react-icons/gi";
 import { BsFillCalendarXFill, BsBoxSeam } from "react-icons/bs";
 import alarmsStyle from "../styles/alarms.module.css";
 import moment from "moment";
+import {
+  getAllCountries,
+  getProductsByCountryAndWarehouse,
+  getWarehouses,
+} from "../lib/productsAPI";
+import CustomSelect from "./customSelect";
 
 function AlarmsList() {
+  const { t } = useTranslation("alarms");
   const router = useRouter();
-  const [allAlarms, setAllAlarms] = useState([]);
+  const [countrySelect, setCountrySelect] = useState(
+    router.query.country || ""
+  );
+  const [warehouseSelect, setWareHouseSelect] = useState(
+    router.query.warehouse || ""
+  );
+  const [productSelect, setProductSelect] = useState(
+    router.query.product || ""
+  );
+  const [countriesList, setCountriesList] = useState([]);
+  const [warehousesList, setWarehousesList] = useState([]);
+  const [productsList, setProductsList] = useState([]);
+  const [alarms, setAlarms] = useState([]);
   const [currentPage, setCurrentPage] = useState(
     parseInt(router.query.page) || 1
   );
-  const { t } = useTranslation("alarms");
-  const itemsPerPage = 10;
-  const [numberOfItems, setNumberOfItems] = useState(null);
+
+  const alarmsPerPage = 10;
+  const [numberOfAlarms, setNumberOfAlarms] = useState(null);
 
   useEffect(() => {
     router.replace({
       query: {
         ...router.query,
+        warehouse: warehouseSelect,
+        country: countrySelect,
+        product: productSelect,
         page: currentPage,
       },
     });
     async function request() {
-      await getAlarms(itemsPerPage, (currentPage - 1) * itemsPerPage).then(
-        (res) => {
-          setNumberOfItems(res.count);
-          setAllAlarms(res.results);
-          console.log(res.results);
-        }
+      const data = await getAlarmsByCountryWarehouseAndProduct(
+        alarmsPerPage,
+        (currentPage - 1) * alarmsPerPage,
+        countrySelect,
+        warehouseSelect,
+        productSelect
       );
+      const warehouses = await getWarehouses(countrySelect, productSelect);
+      const countries = await getAllCountries();
+      const products = await getProductsByCountryAndWarehouse(
+        countrySelect,
+        warehouseSelect
+      );
+      setWarehousesList(warehouses.data);
+      setCountriesList(countries.data);
+      setNumberOfAlarms(data.count);
+      setAlarms(data.results);
+      setProductsList(products.data.results);
     }
     request();
-  }, [currentPage]);
+  }, [currentPage, countrySelect, warehouseSelect, productSelect]);
 
   return (
     <>
-      {allAlarms.length !== 0 ? (
+      <div className="flex flex-col items-center w-[95] mb-10">
+        <CustomSelect
+          items={countriesList}
+          handleSelectItem={setCountrySelect}
+          selectItem={countrySelect}
+          defaultValue={t("country")}
+          setCurrentPage={setCurrentPage}
+          keyOne={"country"}
+          keyTwo={"country"}
+        />
+        <CustomSelect
+          items={warehousesList}
+          handleSelectItem={setWareHouseSelect}
+          selectItem={warehouseSelect}
+          defaultValue={t("warehouse")}
+          setCurrentPage={setCurrentPage}
+          keyOne={"id"}
+          keyTwo={"name"}
+        />
+        <CustomSelect
+          items={productsList}
+          handleSelectItem={setProductSelect}
+          selectItem={productSelect}
+          defaultValue={t("product")}
+          setCurrentPage={setCurrentPage}
+          keyOne={"id"}
+          keyTwo={"name"}
+        />
+      </div>
+      {alarms.length !== 0 ? (
         <table className={alarmsStyle.table}>
           <thead>
             <tr>
@@ -58,56 +123,54 @@ function AlarmsList() {
             </tr>
           </thead>
           <tbody>
-            {allAlarms.map((alarm) => (
-              <tr key={alarm.id} className="bg-white rounded">
-                <td className={alarmsStyle.tCell}>
+            {alarms.map((alarm) => (
+              <tr key={alarm.id} className="bg-white">
+                <td className={alarmsStyle.tCell + " " + alarmsStyle.tCellLeft}>
                   <input type="checkbox" />
                 </td>
                 <td className={alarmsStyle.tCell}>{alarm.delivery_id}</td>
                 <td className={alarmsStyle.tCell}>
-                  <span className="inline-block">
-                    {alarm.issue_temp ? (
-                      <RiTempColdLine size={20} style={{ color: "#ff455a" }} />
-                    ) : (
-                      ""
-                    )}
-                    {alarm.issue_humidity ? (
-                      <MdWaterDrop size={20} style={{ color: "#ff455a" }} />
-                    ) : (
-                      ""
-                    )}
-                    {alarm.issue_shock ? (
-                      <AiOutlineDashboard
-                        size={20}
-                        style={{ color: "#ff455a" }}
-                      />
-                    ) : (
-                      ""
-                    )}
-                    {alarm.issue_light ? (
-                      <MdLightMode size={20} style={{ color: "#ff455a" }} />
-                    ) : (
-                      ""
-                    )}
-                    {alarm.issue_orientation ? (
-                      <BsBoxSeam size={20} style={{ color: "#ff455a" }} />
-                    ) : (
-                      ""
-                    )}
-                    {alarm.issue_eta ? (
-                      <BsFillCalendarXFill
-                        size={20}
-                        style={{ color: "#ff455a" }}
-                      />
-                    ) : (
-                      ""
-                    )}
-                    {alarm.theft ? (
-                      <GiHandcuffed size={20} style={{ color: "#ff455a" }} />
-                    ) : (
-                      ""
-                    )}
-                  </span>
+                  {alarm.issue_temp ? (
+                    <RiTempColdLine size={20} style={{ color: "#ff455a" }} />
+                  ) : (
+                    ""
+                  )}
+                  {alarm.issue_humidity ? (
+                    <MdWaterDrop size={20} style={{ color: "#ff455a" }} />
+                  ) : (
+                    ""
+                  )}
+                  {alarm.issue_shock ? (
+                    <AiOutlineDashboard
+                      size={20}
+                      style={{ color: "#ff455a" }}
+                    />
+                  ) : (
+                    ""
+                  )}
+                  {alarm.issue_light ? (
+                    <MdLightMode size={20} style={{ color: "#ff455a" }} />
+                  ) : (
+                    ""
+                  )}
+                  {alarm.issue_orientation ? (
+                    <BsBoxSeam size={20} style={{ color: "#ff455a" }} />
+                  ) : (
+                    ""
+                  )}
+                  {alarm.issue_eta ? (
+                    <BsFillCalendarXFill
+                      size={20}
+                      style={{ color: "#ff455a" }}
+                    />
+                  ) : (
+                    ""
+                  )}
+                  {alarm.theft ? (
+                    <GiHandcuffed size={20} style={{ color: "#ff455a" }} />
+                  ) : (
+                    ""
+                  )}
                 </td>
                 <td className={alarmsStyle.tCell}>
                   {moment(alarm.date).format("DD-MM-yyyy")}
@@ -117,7 +180,11 @@ function AlarmsList() {
                 </td>
                 <td className={alarmsStyle.tCell}>{alarm.action_taken}</td>
                 <td className={alarmsStyle.tCell}>{alarm.contact_name}</td>
-                <td className={alarmsStyle.tCell}>{alarm.message}</td>
+                <td
+                  className={alarmsStyle.tCell + " " + alarmsStyle.tCellRight}
+                >
+                  {alarm.message}
+                </td>
               </tr>
             ))}
           </tbody>
@@ -127,7 +194,7 @@ function AlarmsList() {
       )}
       <div className="flex justify-center w-full mt-3 bg-main_bg_color">
         <Pagination
-          index={Math.ceil(numberOfItems / itemsPerPage)}
+          index={Math.ceil(numberOfAlarms / alarmsPerPage)}
           setCurrentPage={setCurrentPage}
           currentPage={currentPage}
         />
--- pages/alarms/index.js
+++ pages/alarms/index.js
@@ -3,13 +3,14 @@ import Layout from "../../components/layout";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import AlarmsList from "../../components/alarmsList";
+import alarmsStyle from "../../styles/alarms.module.css";
 
 const Alarms = () => {
   const { t } = useTranslation("alarms");
   return (
     <Layout>
       <Meta pagetitle={t("pageTitle")} />
-      <h1>{t("title")}</h1>
+      <h1 className={alarmsStyle.head}>{t("title")}</h1>
       <AlarmsList />
     </Layout>
   );
